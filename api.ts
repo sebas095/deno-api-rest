7b6c20b6ce6ed47f34cf0d5ba9ffc248de8d200d
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-07-09 02:00:24 -0500

    ADD - mini api added

---
+++ api.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from "./deps.ts";
+
+const api = new Router();
+
+api.get("/", (req: Request, res: Response) => {
+  res.status(200).send({
+    status: "Deno running!",
+  });
+});
+
+api.get("/:id", (req: Request, res: Response) => {
+  res.status(200).send({
+    param: req.params.id,
+  });
+});
+
+export { api };
